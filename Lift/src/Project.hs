e7699bdbc26eeb5740525bb0cd0f10fbc69b8bae
Roman Molochkov <molochkov2017@gmail.com>
2022-06-03 21:59:26 +0300

    Add : solution3

--- Lift/src/Project.hs
+++ Lift/src/Project.hs
@@ -4,9 +4,6 @@ module Project where
 
 import CodeWorld
 
--- | Elevator button.
-data Button = G_Up | G_Down | G_Stop
-
 -- | Colors.
 data Colors = Red | Grey | White | Pink
 
@@ -18,12 +15,12 @@ colors White = white
 colors Pink  = pink
 
 
-arrow :: Colors -> Picture
-arrow color = colored (colors color) (solidPolygon([(0, -0.4), (0.7, -0.7), (0, 1), (-0.7, -0.7)]))
+sign :: Colors -> Picture
+sign color = colored (colors color) (solidPolygon([(0, -0.4), (0.7, -0.7), (0, 1), (-0.7, -0.7)]))
 
 
-rotatedArrow :: Point -> Double -> Colors-> Picture
-rotatedArrow (x, y) delta color = translated x y (rotated delta( arrow color))
+rotatedSign :: Point -> Double -> Colors-> Picture
+rotatedSign (x, y) delta color = translated x y (rotated delta( sign color))
 
 
 frameOfController :: Picture
@@ -31,7 +28,7 @@ frameOfController = colored (light grey) (solidRectangle 1.9 4.4) <> colored bla
 
 
 elevatorController :: Picture
-elevatorController = rotatedArrow (0, 1) 0 Grey <> rotatedArrow (0, -1) pi Grey <> frameOfController
+elevatorController = rotatedSign (0, 1) 0 Grey <> rotatedSign (0, -1) pi Grey <> frameOfController
 
 
 frameOfButton :: Picture
@@ -39,18 +36,11 @@ frameOfButton = colored grey (solidCircle 1.2) <> colored black (solidCircle 1.4
 
 
 buttonOfMoving :: Double -> Picture
-buttonOfMoving delta = rotatedArrow (0, 0) delta White <> frameOfButton
+buttonOfMoving delta = rotatedSign (0, 0) delta White <> frameOfButton
 
 
 buttonOfStoping :: Picture
 buttonOfStoping = colored red (lettering "Stop") <> frameOfButton
-
-
--- | Render elevator button.
-drawButton :: Button ->Picture
-drawButton G_Up   = buttonOfMoving 0
-drawButton G_Down = buttonOfMoving pi
-drawButton G_Stop = buttonOfStoping
 
 -- | Draw several objects
 -- some distance apart from each other. 
@@ -64,37 +54,14 @@ asSpaced distance drawFunction objects
   | otherwise     = translated distance 0 (drawFunction (head objects)) <> (asSpaced (distance + 3) drawFunction (tail objects))
   
 -- | Elevator mode of operation.
-data Mode = M_Up | M_Down | Idle
+data State = UpMove | DownMove | Idle
 
 -- | Some definition of action for FSM.
-type Action = Button
+data Action = UpButton | DownButton | StopButton
 
 
 -- | Transition by an action to a new state.
-type Transition = (Action, Mode)
-
--- | Render elevator mode.
-drawMode :: Mode -> Picture
-drawMode M_Up   = rotatedArrow (0, 1) 0 Red <> elevatorController <> asSpaced 3 drawButton [G_Stop]
-drawMode M_Down = rotatedArrow (0, -1) 0 Red <> elevatorController <> asSpaced 3 drawButton [G_Stop]
-drawMode Idle   = elevatorController <> asSpaced 3 drawButton [G_Up,G_Down]
-
--- | Initial state of an FSM.
-initialState :: Mode
-initialState = Idle
-    
--- | All transitions for a given state.
-elevator :: Mode -> [Transition]
-elevator Idle   = [(G_Up, M_Up), (G_Down, M_Down)]
-elevator M_Up   = [(G_Stop, Idle)]
-elevator M_Down = [(G_Stop, Idle)]
-
--- | Predicate checking if a given state is final.
-isSame :: Action -> Action -> Bool
-isSame G_Up G_Up = True
-isSame G_Down G_Down = True
-isSame G_Stop G_Stop = True
-isSame _ _ = False
+type Transition = (Action, State)
 
 -- | Apply an action (if any) to the current state
 -- of a finite state machine.
@@ -102,69 +69,138 @@ applyAction
   :: Maybe a          -- ˆ An action to apply (if any).
   -> (a -> a -> Bool) -- ˆ Action equality test.
   -> (s -> [(a, s)])  -- ˆ State transitions.
-  -> s                -- ˆ Current state.
-  -> s                -- ˆ New state (if possible).
-applyAction (Just action) isS transitionOf curMode
-  | length transitions == 0 = curMode
+  -> s               -- ˆ Current state.
+  -> s              -- ˆ New state (if possible).
+  
+applyAction Nothing _isS _transitionOf state = state
+applyAction (Just action) isSame transitionOf state
+  | length transitions == 0 = state
+  | length recursiveLangs == 0 = state
   | otherwise = snd (head recursiveLangs)
     where
-      transitions = (transitionOf curMode)
+      transitions = (transitionOf state)
       recursiveLangs = filter (\e -> transitionLang e) transitions
-      transitionLang (ac, _sc) = isS action ac
-        
-applyAction Nothing _isS _transitionOf curMode = curMode
-
-run :: IO ()
-run = do
-  activityOf initialWorld handleWorld renderWorld
-    where    
-      -- | Start of the actor.
-      initialWorld :: Mode
-      initialWorld = Idle
+      transitionLang (ac, _sc) = isSame action ac
       
+-- | Interactive finite state machine simulation.
+interactiveFSM
+  :: s                  -- ˆ Initial state.
+  -> (a -> a -> Bool)   -- ˆ Action equality test.
+  -> (s -> [(a, s)])    -- ˆ State transitions.
+  -> (Event -> Maybe a) -- ˆ How to convert events into actions.
+  -> (s -> Picture)     -- ˆ How to draw states.
+  -> (a -> Picture)     -- ˆ How to draw actions.
+  -> IO ()
+interactiveFSM initState isSame elevator converterOf drawState _drawAction = 
+  activityOf initState handleWorld renderWorld
+    where
       -- | Changing the world and position of the actor and the world
-      handleWorld :: Event -> Mode -> Mode
-      handleWorld (TimePassing dt) mode   = (updateWorld dt mode)
-      handleWorld (PointerPress (3,0)) Idle = applyAction (Just G_Up) isSame elevator Idle
-      handleWorld (PointerPress (6,0)) Idle = applyAction (Just G_Down) isSame elevator Idle
-      handleWorld (PointerPress (3,0)) M_Up  = applyAction (Just G_Stop) isSame elevator M_Up
-      handleWorld (PointerPress (3,0)) M_Down  = applyAction (Just G_Stop) isSame elevator M_Down
-      handleWorld _anyEvent mode          = mode
+      handleWorld :: Event -> s -> s
+      handleWorld (KeyPress key) state = (applyAction (converterOf (KeyPress key)) isSame elevator state)
+      handleWorld _anyEvent state = state
      
       -- | Render actor and world with new data.
-      renderWorld :: Mode -> Picture
-      renderWorld mode = drawMode mode
-    
-    
-      -- | Update world by time.
-      updateWorld :: Double -> Mode -> Mode
-      updateWorld _dt = id
+      renderWorld :: s -> Picture
+      renderWorld state = (drawState state)
+      
 
+run :: IO ()
+run = solution3
 
 solution1 :: IO ()
-solution1 = drawingOf (elevatorController <> asSpaced 5 drawButton [G_Up,G_Down,G_Stop] )
+solution1 = do
+  drawingOf (elevatorController <> asSpaced 5 drawAction [UpButton,DownButton,StopButton])
+      where
+        -- | Render elevator button.
+        drawAction :: Action -> Picture
+        drawAction UpButton   = buttonOfMoving 0
+        drawAction DownButton = buttonOfMoving pi
+        drawAction StopButton = buttonOfStoping
 
 solution2 :: IO ()
 solution2 = do
   activityOf initialWorld handleWorld renderWorld
     where    
       -- | Start of the actor.
-      initialWorld :: Mode
+      initialWorld :: State
       initialWorld = Idle
       
       -- | Changing the world and position of the actor and the world
-      handleWorld :: Event -> Mode -> Mode
-      handleWorld (TimePassing dt) mode   = (updateWorld dt mode)
-      handleWorld (KeyPress "Up") mode    = applyAction (Just G_Up) isSame elevator mode
-      handleWorld (KeyPress "Down") mode  = applyAction (Just G_Down) isSame elevator mode
-      handleWorld (KeyPress " ") mode     = applyAction (Just G_Stop) isSame elevator mode
-      handleWorld _anyEvent mode          = mode
+      handleWorld :: Event -> State -> State
+      handleWorld (KeyPress key) state    
+        | key == "Up"   = applyAction (Just UpButton) isSame elevator state
+        | key == "Down" = applyAction (Just DownButton) isSame elevator state
+        | key == " "    = applyAction (Just StopButton) isSame elevator state
+        | otherwise     = state
+      handleWorld _anyEvent state = state
      
       -- | Render actor and world with new data.
-      renderWorld :: Mode -> Picture
-      renderWorld mode = drawMode mode
+      renderWorld :: State -> Picture
+      renderWorld state = drawState state
     
+      -- | Predicate checking if a given state is final.
+      isSame :: Action -> Action -> Bool
+      isSame UpButton UpButton     = True
+      isSame DownButton DownButton = True
+      isSame StopButton StopButton = True
+      isSame _ _ = False
+      
+      -- | All transitions for a given state.
+      elevator :: State -> [Transition]
+      elevator Idle   = [(UpButton, UpMove), (DownButton, DownMove)]
+      elevator UpMove   = [(StopButton, Idle)]
+      elevator DownMove = [(StopButton, Idle)]
+      
+      -- | Render elevator mode.
+      drawState :: State -> Picture
+      drawState UpMove   = rotatedSign (0, 1) 0 Red <> elevatorController <> asSpaced 3 drawAction [StopButton]
+      drawState DownMove = rotatedSign (0, -1) pi Red <> elevatorController <> asSpaced 3 drawAction [StopButton]
+      drawState Idle   = elevatorController <> asSpaced 3 drawAction [UpButton,DownButton]
+      
+      -- | Render elevator button.
+      drawAction :: Action ->Picture
+      drawAction UpButton   = buttonOfMoving 0
+      drawAction DownButton = buttonOfMoving pi
+      drawAction StopButton = buttonOfStoping
+
+
+solution3 :: IO ()
+solution3 = do
+  interactiveFSM initState isSame elevator converter drawState drawAction
+    where    
+       -- | Initial state of an FSM.
+      initState :: State
+      initState = Idle
+      
+      -- | Predicate checking if a given state is final.
+      isSame :: Action -> Action -> Bool
+      isSame UpButton UpButton     = True
+      isSame DownButton DownButton = True
+      isSame StopButton StopButton = True
+      isSame _ _ = False
+      
+      -- | All transitions for a given state.
+      elevator :: State -> [Transition]
+      elevator Idle   = [(UpButton, UpMove), (DownButton, DownMove)]
+      elevator UpMove   = [(StopButton, Idle)]
+      elevator DownMove = [(StopButton, Idle)]
+      
+      -- | Changing the world and position of the actor and the world
+      converter :: Event -> Maybe Action
+      converter (KeyPress "Up")    = Just UpButton
+      converter (KeyPress "Down")  = Just DownButton
+      converter (KeyPress " ")     = Just StopButton
+      converter _anyEvent          = Nothing
+     
     
-      -- | Update world by time.
-      updateWorld :: Double -> Mode -> Mode
-      updateWorld _dt = id
+      -- | Render elevator mode.
+      drawState :: State -> Picture
+      drawState UpMove   = rotatedSign (0, 1) 0 Red <> elevatorController <> asSpaced 3 drawAction [StopButton]
+      drawState DownMove = rotatedSign (0, -1) pi Red <> elevatorController <> asSpaced 3 drawAction [StopButton]
+      drawState Idle   = elevatorController <> asSpaced 3 drawAction [UpButton,DownButton]
+
+      -- | Render elevator button.
+      drawAction :: Action -> Picture
+      drawAction UpButton   = buttonOfMoving 0
+      drawAction DownButton = buttonOfMoving pi
+      drawAction StopButton = buttonOfStoping
